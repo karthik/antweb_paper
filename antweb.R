b142c0bf81e32cb284a2b3769cb7f71f8e248d60
Karthik Ram <karthik.ram@gmail.com>
2014-09-11 15:15:43 -0700

    Now query all Madagascar data

--- antweb.R
+++ antweb.R
@@ -11,13 +11,18 @@ species <- species$count
 madagascar <- aw_data(country = "Madagascar")
 total_results <- madagascar$count
 offset <- seq(0,ceiling(total_results), by = 1000)
+pb <- txtProgressBar(min = 0, max = length(offset), style = 3)
 madagascar_all <- lapply(offset, function(x) {
 		message(sprintf("finishing %s results", x))
 		output <- aw_data(country = "Madagascar", offset = x, quiet = TRUE)
+		setTxtProgressBar(pb, i)
 		return(output$data)
+
 })
+close(pb)
 all_madagascar_data <- data.table::rbindlist(madagascar_all)
 save(all_madagascar_data, file = "data/madagascar.rda", compress = "xz")
+
 
 
 ## @knitr elevation_gradient
